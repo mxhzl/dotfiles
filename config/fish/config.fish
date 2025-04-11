8d212ac66583e3a610196e3f692079955b9fc884
Maxine <maxine@mxhzl.com>
2025-04-11 16:38:02 -0400

    consolidate dotfiles_local repo into main repo
    I think I've figured out a way to use one repo for
    all the config I need without needing a _local repo

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,17 +15,21 @@ test -e $HOME/.1password/agent.sock && set -gx SSH_AUTH_SOCK $HOME/.1password/ag
 
 set -gx fisher_path $__fish_config_dir/fisher
 
-command -q codium && set -x EDITOR codium -wn
+if command -q codium 
+    set -x EDITOR codium -wn
+    abbr c codium -n
+end
 
 if command -q starship
     starship init fish --print-full-init | source
     enable_transience
 end
 
-command -q zoxide && zoxide init fish | source
+if command -q zoxide 
+    zoxide init fish | source
+    abbr cd z
+end
 
-abbr cd z
-abbr c codium -n
 abbr be bundle exec
 abbr rf source $__fish_config_dir/config.fish
 abbr cp cp -iv
@@ -33,6 +37,8 @@ abbr gst git status
 abbr mkdir mkdir -p
 abbr mv mv -iv
 abbr rm rm -iv
+abbr g git
+abbr b brew
 
 if test -e $__fish_config_dir/config.fish.local
     source $__fish_config_dir/config.fish.local
