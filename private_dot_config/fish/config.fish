a896e3ddca779639555d5a6cb6d42f0e1687e337
Maxine <maxine@mxhzl.com>
2025-04-11 20:12:31 -0400

    switch back to nvim for $editor needs

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -15,10 +15,8 @@ test -e $HOME/.1password/agent.sock && set -gx SSH_AUTH_SOCK $HOME/.1password/ag
 
 set -gx fisher_path $__fish_config_dir/fisher
 
-if command -q codium
-    set -x EDITOR codium -wn
-    abbr c codium -n
-end
+set -x EDITOR nvim
+set -x VISUAL $EDITOR
 
 if command -q starship
     starship init fish --print-full-init | source
@@ -40,6 +38,7 @@ abbr rm rm -iv
 abbr g git
 abbr b brew
 abbr cz chezmoi
+abbr c codium -n
 
 if test -e $__fish_config_dir/config.fish.local
     source $__fish_config_dir/config.fish.local
